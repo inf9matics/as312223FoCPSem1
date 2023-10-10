e7e8ea8b2204b0409616b1a4cfb51595f16aebba
trabus322 <aadam.straczek@gmail.com>
2023-10-10 14:36:31 +0200

    Point 1 done

--- lab-02/main.cpp
+++ lab-02/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
-using namespace std;
+
 int main() // definition of the main function
 {
-   int a, b; // declaration of two variables of type int (integer numbers)
-   a = 11;  // assigning value 11 to the variable a
-   b = 33;
-   std::cout << a << " + " << b << " = " << a + b << std::endl;  // 11 + 33 = 44
-   return 0;
+    int a, b; // declaration of two variables of type int (integer numbers)
+
+    a = 11;  // assigning value 11 to the variable a
+    b = 33;
+    std::cout << "Product of " << a << " and " << b << " is " << a*b << std::endl;
+    
+    a = (float) a; //a is casted to float, you can't change the type of defined variable
+    std::cout << "Input a number for 'a': ";
+    std::cin >> a;
+
+    std::cout << "\'a\' and \'b\' become a char, so they\'re written out as the ASCII representation: " << a << ':' << (char) a << ' ' << b << ':' << (char) b << std::endl;
+
+    
+
+    return 0;
 }
