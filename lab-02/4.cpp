ef1ddceace2669cf82ee86aa8e4ed3c0694c644c
trabus322 <aadam.straczek@gmail.com>
2023-10-10 15:45:20 +0200

    Started exercise 4

---
+++ lab-02/4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+    int a, b, c;
+    std::cout << "Quadratic equation solver" << std::endl;
+    std::cout << "Input a: ";
+    std::cin >> a;
+    std::cout << "Input b: ";
+    std::cin >> b;
+    std::cout << "Input c: ";
+    std::cin >> c;
+
+    float delta = pow(a, 2);
+
+    return 0;
+}
