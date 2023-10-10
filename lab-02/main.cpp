3bf8188889d09439a816d7f8890f1cc00c87fea3
trabus322 <aadam.straczek@gmail.com>
2023-10-10 14:20:51 +0200

    Initial commit for lab-02

---
+++ lab-02/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() // definition of the main function
+{
+   int a, b; // declaration of two variables of type int (integer numbers)
+   a = 11;  // assigning value 11 to the variable a
+   b = 33;
+   std::cout << a << " + " << b << " = " << a + b << std::endl;  // 11 + 33 = 44
+   return 0;
+}
