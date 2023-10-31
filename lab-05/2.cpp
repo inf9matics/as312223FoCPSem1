5a9388ee3fb9ed8f846ff0199fd769b651e555d7
trabus322 <aadam.straczek@gmail.com>
2023-10-31 15:37:51 +0100

    Failed in creating own pure C array with the intent of assigning size later

--- lab-05/2.cpp
+++ lab-05/2.cpp
@@ -17,6 +17,50 @@ float maxOf3(float a, float b, float c){
     }
 }
 
+float maxOf7(float a, float b, float c, float d, float e, float f, float g){
+    float ab;
+    float cd;
+    float ef;
+    if(a>b){
+        ab = a;
+    }
+    else{
+        ab = b;
+    }
+    if(c>d){
+        cd = c;
+    }
+    else{
+        cd = d;
+    }
+    if(e>f){
+        ef = e;
+    }
+    else{
+        ef = f;
+    }
+    float abcd;
+    float efg;
+    if(ab>cd){
+        abcd = ab;
+    }
+    else{
+        abcd = cd;
+    }
+    if(ef>g){
+        efg = ef;
+    }
+    else{
+        ef = g;
+    }
+    if(abcd > efg){
+        return abcd;
+    }
+    else{
+        return efg;
+    }
+}
+
 int main(){
     float a {1.387};
     float b {-7.643};
